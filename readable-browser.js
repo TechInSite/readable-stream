b2879b8560b91c273209cb1a0b6cfdafa00dfaa2
Kevin Brown <kevin.brown@techin.site>
2021-06-05 21:12:08 +1000

    Moving to Registry pattern for Rollup.

--- readable-browser.js
+++ readable-browser.js
@@ -1,9 +1,19 @@
-exports = module.exports = require('./lib/_stream_readable.js');
-exports.Stream = exports;
-exports.Readable = exports;
-exports.Writable = require('./lib/_stream_writable.js');
-exports.Duplex = require('./lib/_stream_duplex.js');
-exports.Transform = require('./lib/_stream_transform.js');
-exports.PassThrough = require('./lib/_stream_passthrough.js');
+const Registry = require('./lib/_registry.js');
+
+require('./lib/_stream_readable.js');
+require('./lib/_stream_writable.js');
+require('./lib/_stream_duplex.js');
+require('./lib/_stream_transform.js');
+require('./lib/_stream_passthrough.js');
+require('./lib/_stream_passthrough.js');
+
+exports = module.exports = Registry.Readable;
+
+exports.Stream = Registry.Readable;
+exports.Readable = Registry.Readable;
+exports.Writable = Registry.Writable;
+exports.Duplex = Registry.Duplex;
+exports.Transform = Registry.Transform;
+exports.PassThrough = Registry.PassThrough;
 exports.finished = require('./lib/internal/streams/end-of-stream.js');
 exports.pipeline = require('./lib/internal/streams/pipeline.js');
